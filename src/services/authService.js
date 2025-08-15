d1a2302ad2f7a813f754f6a15c7f2efb71b16172
Kallam Taylor <kallam_taylor@hotmail.com>
2025-08-15 02:47:00 +0100

    userService.js added

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,4 +1,3 @@
-import axios from 'axios'
 const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/api/auth`
 
 const signUp = async (formData) => {
---
+++ src/services/userService.js
@@ -0,0 +1,28 @@
+const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}`
+
+const index = async () => {
+    try {
+        const token = localStorage.getItem('token')
+
+        if (!token) throw new Error('No Token Found')
+        
+        const res = await fetch(`${BASE_URL}/users`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+
+                'Authorization': `Bearer ${token}`
+            }
+        })
+
+        const data = await res.json()
+
+        if (data.err) {
+            throw new Error(data.err)
+        }
+        return data
+    } catch (err) {
+        console.log(err)
+        throw new Error(err.message)
+    }
+}
